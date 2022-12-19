68b812ccebd32e242a8c1d0647cf8f12d48d2978
Chandana B S <84517341+chandanabs13@users.noreply.github.com>
2022-12-19 23:20:54 +0530

    Create stringify.js

---
+++ stringify.js
@@ -0,0 +1,8 @@
+//json stringify
+//object stringify
+var object={name: 'John', age: 30, city: 'New York'};
+console.log(JSON.stringify(object));
+
+//array stringify
+var array=[1,2,3,4,5,6,7,8,9];
+console.log(JSON.stringify(array));
